c957f8ae77d91da967985f45be9afe0d3ac1e650
sharmi <introvertbeauty7@gmail.com>
2024-09-13 07:42:26 +0530

    elasticbeanstalk

--- application.py
+++ application.py
@@ -19,9 +19,44 @@ def index():
     return render_template("index.html")
 
 
+
+#  Gender	Married	Dependents	Education	Self_Employed	ApplicantIncome	CoapplicantIncome	LoanAmount	Loan_Amount_Term	Credit_History	Property_Area
 @app.route("/predictdata",methods=["GET","POST"])
-def home():
-    pass:
+def predict_datapoint():
+    if request.method=="POST":
+        Gender=int(request.form.get("Gender"))
+        Married=int(request.form.get("Married"))
+        Dependents=int(request.form.get("Dependents"))
+        Education=int(request.form.get("Education"))
+        Self_Employed=int(request.form.get("Self_Employed"))
+        ApplicantIncome=int(request.form.get("ApplicantIncome"))
+        CoapplicantIncome=float(request.form.get("CoapplicantIncome"))
+        LoanAmount=float(request.form.get("LoanAmount"))
+        Loan_Amount_Term=float(request.form.get("Loan_Amount_Term"))
+        Credit_History=float(request.form.get("Credit_History"))
+        Property_Area=int(request.form.get("Property_Area"))
+
+        features=[[Gender,Married,Dependents,Education,Self_Employed,ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,Property_Area]]
+
+
+        # logistic_model=logistic_model.predict(features)
+        new_data_scaled=standard_scaler.transform(features)
+        result=logistic_model.predict(new_data_scaled)
+        print(result)
+
+        if result[0] == 1:
+          result = "Congratulations! Your loan has been approved."
+        else:
+           result = "Sorry, your loan application was not approved."
+
+
+        
+        return render_template("home.html", prediction_text="This User Will get {}".format(result))
+
+        
+        
+    else:
+        return render_template("home.html")
     
 
 
